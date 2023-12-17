f2bcc5f851230979c4bb3f11ff72ca1c3002018b
NACHC-CAD <greshje.nachc@gmail.com>
2023-12-17 17:23:59 -0500

    working out infrastructure

---
+++ R_SOURCE/main/org/ohdsi/util/lib/OhdsiLibUtil.R
@@ -0,0 +1,15 @@
+# ---
+#
+# A class to encapsulate basic library/package related functionality
+#
+# ---
+
+OhdsiLibUtil = R6Class(
+
+    classname = "OhdsiLibUtil",
+
+    public = list (
+        
+    )
+
+)
