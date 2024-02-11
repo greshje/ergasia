0ec917fb7f975be07c75ddde662e5c9136f8688e
NACHC-CAD <greshje.nachc@gmail.com>
2024-02-11 08:57:52 -0500

    work in progress

--- R/impl/reset/ResetResultsDatabaseTables.R
+++ R/impl/reset/ResetResultsDatabaseTables.R
@@ -3,6 +3,24 @@ source("./R/impl/configuration/ConfigurationFactory.R")
 source("./_StartHere/ConnectionDetails/CDM/ConnectionDetailsForCdm.R")
 source("./R/impl/database/ErgDbUtil.R")
 
+# function used to echo what table is being dropped/recreated
+echoStatus <- function(tableName) {
+  msg <- "\n\n\n"
+  msg <- paste0(msg, "\n# ---")
+  msg <- paste0(msg, "\n#")
+  msg <- paste0(msg, "\n# RECREATING TABLE: ", tableName)
+  msg <- paste0(msg, "\n#")
+  msg <- paste0(msg, "\n# ---")
+  writeLines(msg)
+}
+
+# function to drop and recreate a table
+dropAndRecreateTable <- function (tableName) {
+  echoStatus(tableName)
+  ErgDbUtil$exec(paste0("drop schema if exists ", tableName, " cascade"), dbms, conn)
+  ErgDbUtil$exec(paste0("create schema ", config$workSchema), dbms, conn)
+}
+
 # get the configuration and dbms for the cdm
 config <- ConfigurationFactory$getConfiguration()
 dbms <- config$dbms
@@ -16,7 +34,6 @@ success <- ErgDbUtil$query("select 1 as one", dbms, conn)
 print(success)
 
 # reset the working tables
-ErgDbUtil$exec(paste0("drop database if exists ", config$workSchema), dbms, conn)
-ErgDbUtil$exec(paste0("create database ", config$workSchema), dbms, conn)
+dropAndRecreateTable(config$workSchema)
 
 
