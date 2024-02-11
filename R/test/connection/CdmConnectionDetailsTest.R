46a84a00542c53d1a34aecbfd4cbd76e6b95cd7b
NACHC-CAD <greshje.nachc@gmail.com>
2024-02-11 06:58:02 -0500

    adding tests for connections and SqlRender

--- R/test/connection/CdmConnectionDetailsTest.R
+++ R/test/connection/CdmConnectionDetailsTest.R
@@ -8,6 +8,7 @@
 library(testthat)
 
 # imports
+source("./R/impl/configuration/ConfigurationFactory.R")
 source("./R/impl/connection/ConnectionDetailsFactory.R")
 
 # ---
@@ -20,8 +21,10 @@ test_that (
   "Connection can be created for CDM",
   {
     expect_true(1 == 1)
+    config <- ConfigurationFactory$getConfiguration()
+    dbms <- config$dbms
     connectionDetails <- ConnectionDetailsFactory$getCdmConnectionDetails()
-    ConnectionDetailsTester$testConnection(connectionDetails)
+    ConnectionDetailsTester$testConnection(connectionDetails, dbms)
   }
   
 )
